1d835ada304012b2c253bda52eb7eaa9cdf1d837
Nicolas Boyet <nicolas.boyet@gmail.com>
2022-04-27 22:11:27 +0200

    Add flask cors

--- api/app.py
+++ api/app.py
@@ -1,8 +1,9 @@
 from flask import Flask
-
+from flask_cors import CORS
 from config import DevConfig
 
 app = Flask(__name__)
+CORS(app)
 app.config.from_object(DevConfig)
 
 if __name__ == '__main__':
