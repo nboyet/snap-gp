a3c60b532239aba602eb725cc553eaf2fea02610
Nicolas Boyet <nicolas.boyet@gmail.com>
2022-04-27 23:34:33 +0200

    Navbar ok

--- front/src/components/Navbar.tsx
+++ front/src/components/Navbar.tsx
@@ -1,24 +1,37 @@
 /* This example requires Tailwind CSS v2.0+ */
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Disclosure } from "@headlessui/react";
 import { MenuIcon, SunIcon, XIcon, MoonIcon } from "@heroicons/react/outline";
-
-const navigation = [
-  { name: "Dashboard", href: "#", current: true },
-  { name: "Team", href: "#", current: false },
-  { name: "Projects", href: "#", current: false },
-  { name: "Calendar", href: "#", current: false },
-];
-
+import { RoutesBase } from "../constants";
+import { Link, useLocation } from "react-router-dom";
+import logo from "../assets/img/logo.png";
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
 }
-
 export default function Navbar() {
   const [darkTheme, setDarkTheme] = useState(false);
+  const location = useLocation();
+  const [current, setCurrent] = useState(location.pathname);
+
+  useEffect(() => {
+    setCurrent(location.pathname);
+  }, [location]);
+
+  const navigation = [
+    {
+      name: "Topology",
+      href: RoutesBase.HOME,
+      current: current === RoutesBase.HOME,
+    },
+    {
+      name: "Switch",
+      href: RoutesBase.MANAGE,
+      current: current === RoutesBase.MANAGE,
+    },
+  ];
 
   return (
-    <Disclosure as="nav" className="bg-gray-800">
+    <Disclosure as="nav" className="bg-blue-800">
       {({ open }) => (
         <>
           <div className="px-2 sm:px-6 lg:px-8">
@@ -36,33 +49,25 @@ export default function Navbar() {
               </div>
               <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex-shrink-0 flex items-center">
-                  <img
-                    className="block lg:hidden h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                    alt="Workflow"
-                  />
-                  <img
-                    className="hidden lg:block h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
-                    alt="Workflow"
-                  />
+                  <img className="h-8 w-auto" src={logo} alt="SnapGP" />
                 </div>
                 <div className="hidden sm:block sm:ml-6">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
-                      <a
+                      <Link
+                        to={item.href}
                         key={item.name}
-                        href={item.href}
                         className={classNames(
                           item.current
-                            ? "bg-gray-900 text-white"
-                            : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                          "px-3 py-2 rounded-md text-sm font-medium"
+                            ? "border-white text-white "
+                            : "border-transparent ",
+                          "px-3 text-gray-300 border-b-2 hover:border-white py-2  text-sm font-medium hover:text-white transition duration-300"
                         )}
                         aria-current={item.current ? "page" : undefined}
+                        onClick={() => setCurrent(item.href)}
                       >
                         {item.name}
-                      </a>
+                      </Link>
                     ))}
                   </div>
                 </div>
@@ -87,10 +92,9 @@ export default function Navbar() {
           <Disclosure.Panel className="sm:hidden">
             <div className="px-2 pt-2 pb-3 space-y-1">
               {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
+                <Link
+                  to={item.href}
+                  key={item.name + "_mobile"}
                   className={classNames(
                     item.current
                       ? "bg-gray-900 text-white"
@@ -100,7 +104,7 @@ export default function Navbar() {
                   aria-current={item.current ? "page" : undefined}
                 >
                   {item.name}
-                </Disclosure.Button>
+                </Link>
               ))}
             </div>
           </Disclosure.Panel>
